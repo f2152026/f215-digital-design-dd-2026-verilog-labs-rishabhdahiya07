b66231cba636ca67fbd2e9ff0cb5a3e16af59ef8
RishabhDahiya07 <f20250628@pilani.bits-pilani.ac.in>
2026-09-19 09:53:01 +0000

    lab02 task1

--- labs/lab02/task1/mux_beh.v
+++ labs/lab02/task1/mux_beh.v
@@ -9,14 +9,19 @@ module mux_beh (
   input       I0,
   input       I1,
   input       S,
-  output wire Y
+  output reg Y
 );
 
   always @(*) begin
+
     if (S)
+
       Y = I1;
+
     else
+
       Y = I0;
+
   end
 
 endmodule
